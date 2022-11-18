5313748721bd50e0bb9bba8e99b3198dad388899
lhenningson <l.henningson@share.epsb.ca>
2022-11-18 11:21:55 -0700

    uryrt

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Script.js
+let calcBtn = document.getElementById("calc-btn");
+let sumout = document.getElementById("sum-out");
+let input = document.getElementById("n-in");
+let val = input.value;
+
+calcBtn.addEventListener("click", calcsum);
+
+
+function calcsum() { 
+    console.log(val)
+    let total = 0;
+    for (let n = 1; n <= v; n++) {
+        total = total + n;
+    }
+    sumout.innerHTML = total;
+}
